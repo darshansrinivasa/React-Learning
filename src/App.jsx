12ab6819a611d2ad9e1e3ebd6ffed8a76b000183
Darshan <darshan.s@ecomexperts.io>
2024-12-04 19:50:22 +0530

    JSX Attributes and Styling React Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
+import indian from './assets/Indian.jpeg'
+import italian from './assets/Italian.jpeg'
+import french from './assets/french.jpeg'
 
 const App = () => {
   const name = "Darshan S";
   const year = new Date().getFullYear();
   return (
-    <>
-      <p>Created by {name}</p>
-      <p>Copyright {year}</p>
-    </>
+    <div>
+      <div>
+        <h1 className='heading'>My favourite Food</h1>
+        <ul>
+          <li>Indian <span>-</span> <img className='image' src={ indian } /> </li>
+          <li>French <span>-</span> <img className='image' src={italian} /></li>
+          <li>Italian <span>-</span> <img className='image' src={french} /></li>
+        </ul>
+      </div>
+      <div>
+        <p>Created by {name}</p>
+        <p>Copyright {year}</p>
+      </div>
+    </div>
   )
 }
 
