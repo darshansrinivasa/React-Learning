bdeb1a2b13c8e5cdb0c62892fe9e29aff4a18e13
Darshan <darshan.s@ecomexperts.io>
2024-12-04 20:23:07 +0530

    React styling practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import './App.css'
-import indian from './assets/Indian.jpeg'
-import italian from './assets/Italian.jpeg'
-import french from './assets/french.jpeg'
 
 const App = () => {
-  const name = "Darshan S";
-  const year = new Date().getFullYear();
+
+  let greetText;
+  const date = new Date("January 01, 2025 18:00:00"); // update time to check different conditions
+  const time = date.getHours();
   const customStyle = {
-    fontSize: "50px",
-    color: "#f0f0f0",
-    border: "1px solid #f0f0f0"
+    color: ""
   }
 
-  customStyle.color = "yellow";
+  if (time >= 0 && time < 12) {
+    greetText = 'Good Morning';
+    customStyle.color = 'red'
+  } else if (time > 12 && time < 18) {
+    greetText = 'Good Afternoon';
+    customStyle.color = 'green'
+  } else {
+    greetText = 'Good Night';
+    customStyle.color = 'blue'
+  }
 
   return (
     <div>
       <div>
-        <h1 style={customStyle}>Hello World</h1>
-        <h2 className='heading'>My favourite Food</h2>
-        <ul>
-          <li>South Indian <span>-</span> <img className='image' src={ indian } /> </li>
-          <li>French <span>-</span> <img className='image' src={italian} /></li>
-          <li>Italian <span>-</span> <img className='image' src={french} /></li>
-        </ul>
-      </div>
-      <div>
-        <p>Created by {name}</p>
-        <p>Copyright {year}</p>
+        <h1 style={customStyle}> { greetText } </h1>
+        { time }
       </div>
     </div>
   )
