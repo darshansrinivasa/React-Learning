4005cbd4d44f3263f28dec00a3d67e5f5a732478
Darshan <darshan.s@ecomexperts.io>
2024-12-04 20:09:19 +0530

    Inline styling for react elements

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,21 @@ import french from './assets/french.jpeg'
 const App = () => {
   const name = "Darshan S";
   const year = new Date().getFullYear();
+  const customStyle = {
+    fontSize: "50px",
+    color: "#f0f0f0",
+    border: "1px solid #f0f0f0"
+  }
+
+  customStyle.color = "yellow";
+
   return (
     <div>
       <div>
-        <h1 className='heading'>My favourite Food</h1>
+        <h1 style={customStyle}>Hello World</h1>
+        <h2 className='heading'>My favourite Food</h2>
         <ul>
-          <li>Indian <span>-</span> <img className='image' src={ indian } /> </li>
+          <li>South Indian <span>-</span> <img className='image' src={ indian } /> </li>
           <li>French <span>-</span> <img className='image' src={italian} /></li>
           <li>Italian <span>-</span> <img className='image' src={french} /></li>
         </ul>
